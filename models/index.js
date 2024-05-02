28f9046c941c6390780c9250d03ac09f8f97ea26
Sarah Higley <sarah.higley19@gmail.com>
2024-05-01 18:33:29 -0600

    changing naming schema

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const User = require('./User');
-const Post = require('./Post');
+const User = require('./User1');
+const Post = require('./Post1');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
